5c8d3a9350adbec24191fe3cd94ebf192cce7b94
Nithin Meppurathu <nithin@spinmedia.com>
2016-02-03 13:31:18 -0500

    moved out ssh into its own rep, i use it way too often

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
-	"io"
 	"math/rand"
 	"os"
 	"regexp"
@@ -12,7 +10,7 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/chonthu/rtail/easyssh"
+	"github.com/chonthu/ssh"
 	"github.com/fatih/color"
 )
 
@@ -122,7 +120,7 @@ func Connect(server string) {
 	fmt.Printf("Connecting to %v as %v \n", c(server), user)
 
 	// Create MakeConfig instance with remote username, server address and path to private key.
-	ssh := &easyssh.MakeConfig{
+	s := &ssh.MakeConfig{
 		User:   user,
 		Server: server,
 		// Optional key or Password without either we try to contact your agent SOCKET
@@ -136,7 +134,7 @@ func Connect(server string) {
 	}
 
 	// Call Run method with command you want to run on remote server.
-	channel, done, err := Stream(ssh, fmt.Sprintf("tail %v %v", follow, fileToLog))
+	channel, done, err := ssh.Stream(s, fmt.Sprintf("tail %v %v", follow, fileToLog))
 	if err != nil {
 		fmt.Errorf("Stream failed: %s", err)
 	}
@@ -149,42 +147,4 @@ func Connect(server string) {
 			fmt.Printf("[%s] %s\n", c(server), line)
 		}
 	}
-}
-
-// Stream returns one channel that combines the stdout and stderr of the command
-// as it is run on the remote machine, and another that sends true when the
-// command is done. The sessions and channels will then be closed.
-func Stream(ssh_conf *easyssh.MakeConfig, command string) (output chan string, done chan bool, err error) {
-	// connect to remote host
-	session, err := ssh_conf.Connect()
-	if err != nil {
-		return output, done, err
-	}
-	// connect to both outputs (they are of type io.Reader)
-	outReader, err := session.StdoutPipe()
-	if err != nil {
-		return output, done, err
-	}
-	errReader, err := session.StderrPipe()
-	if err != nil {
-		return output, done, err
-	}
-	// combine outputs, create a line-by-line scanner
-	outputReader := io.MultiReader(outReader, errReader)
-	err = session.Start(command)
-	scanner := bufio.NewScanner(outputReader)
-	// continuously send the command's output over the channel
-	outputChan := make(chan string)
-	done = make(chan bool)
-	go func(scanner *bufio.Scanner, out chan string, done chan bool) {
-		defer close(outputChan)
-		defer close(done)
-		for scanner.Scan() {
-			outputChan <- scanner.Text()
-		}
-		// close all of our open resources
-		done <- true
-		session.Close()
-	}(scanner, outputChan, done)
-	return outputChan, done, err
 }
