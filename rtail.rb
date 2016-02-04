1f53c64a03ac745d2abb45d7f5165f0291a752b1
Nithin Meppurathu <admin@nitmedia.com>
2016-02-03 21:27:31 -0500

    homebrew package

---
+++ rtail.rb
@@ -0,0 +1,39 @@
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                http://www.rubydoc.info/github/Homebrew/homebrew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Rtail < Formula
+  desc "Remote tail logging"
+  homepage "https://github.com/chonthu/rtail"
+  url "https://s3.amazonaws.com/rlsr/rtail/darwin/0.0.1/rtail.gz"
+  version "0.0.1"
+  sha256 "bf9dc8fffcb003731e4bb92646ce541f4b5e0e7dfafdc08a0825cee578de173f"
+
+  # depends_on "cmake" => :build
+  depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test rtail`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
