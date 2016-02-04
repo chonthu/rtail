cedd59696fd63bf7b5bb83935d35b515929e88a7
Nithin Meppurathu <admin@nitmedia.com>
2016-02-03 21:30:37 -0500

    cleaning up uneeded configs

--- rtail.rb
+++ rtail.rb
@@ -14,14 +14,6 @@ class Rtail < Formula
 
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
-
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
   end
 
   test do
